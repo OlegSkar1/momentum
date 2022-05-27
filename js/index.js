5f9760f26e2328782ac9829eed2bf54cbf25c8e2
Oleg Skarednov <olegskar1@gmail.com>
2022-05-28 01:07:23 +0500

    feat greeting

--- js/index.js
+++ js/index.js
@@ -12,10 +12,24 @@ function showDate() {
   const currentDate = new Date().toLocaleDateString('ru-RU', options);
   currentDay.textContent = currentDate;
 }
-function showTime() {
+setTimeout(function showTime() {
   const date = new Date().toLocaleTimeString();
   timer.textContent = date;
   showDate();
+  greeting.textContent = greetingText;
   setTimeout(showTime, 1000);
+}, 1000);
+
+//Приветствие
+const greeting = document.querySelector('.greeting');
+const hours = new Date().getHours();
+const timeOfDay = getTimeOfDay();
+const greetingText = `Good ${timeOfDay}`;
+
+function getTimeOfDay() {
+  let partOfDay = hours / 6;
+  if (partOfDay < 1) return 'night';
+  else if (partOfDay >= 1 && partOfDay < 2) return 'morning';
+  else if (partOfDay >= 2 && partOfDay < 3) return 'day';
+  else return 'evening';
 }
-setTimeout(showTime, 1000);
